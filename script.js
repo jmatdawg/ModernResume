4be541405b841ff6cbeb5c5eba3ad2c5a315591d
jmatdawg <matthews_jakiera@yahoo.com>
2018-01-17 17:07:16 -0600

    resume update

---
+++ script.js
@@ -0,0 +1,50 @@
+var str = "jakiera matthews-lucy"
+var res = str.toUpperCase();
+console.log('Name: ' + res)
+
+console.log('Career: Mathematician')
+
+console.log('Description: I am the bomb like tick, tick.')
+
+console.log('My Interests:')
+
+console.log('* Jesus')
+
+console.log('* DIY-ing')
+
+console.log('* Attempting recipes from Pinterest')
+
+console.log('My Previous Experience:')
+
+var displayPosition( jobtitle, companyname, description) {
+    console log(`* ${jobtitle} at ${companyname} - ${description})
+
+}
+
+console.log('* Math Teacher at St. Barnabas Catholic School')
+
+console.log('- Taught middle school mathematics curriculum')
+
+console.log('* Financial Services Professional at Old National Bank')
+
+console.log('- Explored Fraud Investigations, Retail Banking, and Retail Data Analytics of a large financial institution')
+
+console.log('* Student Tutor at Alabama State University Quant Resource Center')
+
+console.log('- Tutored and provided assessments to ASU students in college-level math courses')
+
+console.log('My Skills')
+
+console.log('* Dancing')
+
+console.log('* Check It: Backflips')
+
+console.log('* Public Speaking')
+
+console.log('* Painting')
+
+console.log('* Check It: Gin Rummy')
+
+console.log('* Check It: Opera Singing')
+
+console.log('* Check It: Changing Tires')
